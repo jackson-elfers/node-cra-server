308a0e172a1a5ef37487c94ede454eddf129993a
jackson-elfers <jacksonelfersdev@outlook.com>
2019-11-23 19:41:07 -0800

    add: reorganize client and install react-router-dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { Home, NotFound } from "./pages";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
