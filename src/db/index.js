d48ef6c0dfb0903c7b6218e8bcf673f07bf3faf0
jackson-elfers <jacksonelfersdev@outlook.com>
2020-01-25 13:11:40 -0800

    add: query error handling and info object

--- src/db/index.js
+++ src/db/index.js
@@ -1,3 +1,4 @@
+const check = require("check-types");
 const mysql = require("mysql");
 const models = require("./models");
 
@@ -14,13 +15,22 @@ module.exports.connect = function() {
   });
 };
 
-module.exports.query = function(string) {
+module.exports.query = function(string, info) {
+  if (info === undefined) {
+    info = null;
+  } else {
+    check.assert(check.object(info), "optional second argument must be object");
+  }
   return new Promise((resolve, reject) => {
     connection.query(string, function(error, results, fields) {
       if (error) {
         reject(error);
       } else {
-        resolve({ results: results, fields: fields });
+        resolve({
+          results: results,
+          fields: fields === undefined ? null : fields,
+          info: info
+        });
       }
     });
   });
