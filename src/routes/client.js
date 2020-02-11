24419a3603e68952ce5b7cac8f555aef80605a04
jackson-elfers <jacksonelfersdev@outlook.com>
2020-02-11 10:05:51 -0800

    add: organized routes for tests

--- src/routes/client.js
+++ src/routes/client.js
@@ -1,10 +1,5 @@
 const path = require("path");
 
-function home(req, res) {
+module.exports.home = function(req, res) {
   res.sendFile(path.join(process.cwd(), "./client/build/index.html"));
-}
-
-module.exports = function(app) {
-  app.get("/", home);
-  app.get("*", home);
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,8 @@
+const utils = require("../utils");
+const client = require("./client");
+
 module.exports = function(app) {
-  require("./client")(app);
+  // client
+  app.get("/", client.home);
+  app.get("*", client.home);
 };
